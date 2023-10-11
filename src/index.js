870c9196e487c536bbee63693b08b86cfbd4129c
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-11 09:18:49 +0300

    exercise name viscibility

--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function getAllExercises() {
-        fetch("https://api.api-ninjas.com/v1/exercises", {
-            method: 'GET',
-            headers: {
-                "X-Api-Key": "EPc0NkSNg0JxRXN5d+E7Sg==g0SNWTUmVNJnZgeK",
-            },
-        })
-            .then((response) => response.json())
-            .then((exercises) => {
-                exercises.forEach((exercise) => {
-                    // Print each exercise's details
-                    console.log(exercise);
-                });
-            })
-            .catch((error) => console.log("error", error));
-    }
-    function getEachExercises(){
-        exercises.forEach((exercise) => {
-                    // Print each exercise's details
-                    console.log(exercise);
-                });
-    }
-    getAllExercises();
-});
+  let id = document.querySelector("#exers")
+  //console.log(id);
+  function getAllExercises() {
+      fetch("https://api.api-ninjas.com/v1/exercises", {
+          method: 'GET',
+          headers: {
+              "X-Api-Key": "EPc0NkSNg0JxRXN5d+E7Sg==g0SNWTUmVNJnZgeK",
+          },
+      })
+          .then((response) => response.json())
+          .then((exercises) => {
+              exercises.forEach((exercise) => {
+                // a new paragraph for each exercise and display the exercise name
+                let p = document.createElement("p");
+                p.innerHTML = exercise.name;
+                id.appendChild(p);
+                p.style.color = "blue"
+                let exerciseDetails = document.createElement('ul')
+                exerciseDetails.className = "details"
+                exerciseDetails.style.color = "#151515"
+                p.appendChild(exerciseDetails)
+                exerciseDetails.innerHTML = `
+                <li>${exercise.type}</li>
+                <li>${exercise.muscle}</li>
+                <li>${exercise.equipment}</li>
+                <li>${exercise.difficulty}</li>
+                <li>${exercise.descrption}</li>
+                `
 
-
-
-
-
-
+p.addEventListener("click",(e)=>{
+  e.preventDefault()
   
-
+})
+            });
+          })
+          .catch((error) => console.log("error", error));
+  }
+  
+  getAllExercises();
+});
