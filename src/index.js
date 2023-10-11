c6e9b0b026a0b71997aa0e231dd076a08fa15d74
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-11 09:30:02 +0300

    add event listener

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let id = document.querySelector("#exers")
+  let id = document.querySelector("#exers");
   //console.log(id);
   function getAllExercises() {
-      fetch("https://api.api-ninjas.com/v1/exercises", {
-          method: 'GET',
-          headers: {
-              "X-Api-Key": "EPc0NkSNg0JxRXN5d+E7Sg==g0SNWTUmVNJnZgeK",
-          },
-      })
-          .then((response) => response.json())
-          .then((exercises) => {
-              exercises.forEach((exercise) => {
-                // a new paragraph for each exercise and display the exercise name
-                let p = document.createElement("p");
-                p.innerHTML = exercise.name;
-                id.appendChild(p);
-                p.style.color = "blue"
-                let exerciseDetails = document.createElement('ul')
-                exerciseDetails.className = "details"
-                exerciseDetails.style.color = "#151515"
-                p.appendChild(exerciseDetails)
-                exerciseDetails.innerHTML = `
+    fetch("https://api.api-ninjas.com/v1/exercises", {
+      method: "GET",
+      headers: {
+        "X-Api-Key": "EPc0NkSNg0JxRXN5d+E7Sg==g0SNWTUmVNJnZgeK",
+      },
+    })
+      .then((response) => response.json())
+      .then((exercises) => {
+        exercises.forEach((exercise) => {
+          // a new paragraph for each exercise and display the exercise name
+          let p = document.createElement("p");
+          p.innerHTML = exercise.name;
+          id.appendChild(p);
+          p.style.color = "blue";
+          // exerciseDetails.style.display === "hidden" and display on click
+          p.addEventListener("click", (e) => {
+            e.preventDefault();
+            let exerciseDetails = document.createElement("ul");
+            exerciseDetails.className = "details";
+            exerciseDetails.style.color = "#151515";
+
+            p.appendChild(exerciseDetails);
+            exerciseDetails.innerHTML = `
                 <li>${exercise.type}</li>
                 <li>${exercise.muscle}</li>
                 <li>${exercise.equipment}</li>
                 <li>${exercise.difficulty}</li>
                 <li>${exercise.descrption}</li>
-                `
-
-p.addEventListener("click",(e)=>{
-  e.preventDefault()
-  
-})
-            });
-          })
-          .catch((error) => console.log("error", error));
+                `;
+          });
+        });
+      })
+      .catch((error) => console.log("error", error));
   }
-  
+
   getAllExercises();
 });
