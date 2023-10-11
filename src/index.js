50f063198a0961d4f30c3ff700377d5df9aeb96a
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-11 07:46:38 +0300

    iterating through objects

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-document.addEventListener("DOMContentLoaded", function(){
-    function getAllExercises(exercises) {
-        fetch("https://api.api-ninjas.com/v1/exercises",{
+document.addEventListener("DOMContentLoaded", function () {
+    function getAllExercises() {
+        fetch("https://api.api-ninjas.com/v1/exercises", {
             method: 'GET',
             headers: {
                 "X-Api-Key": "EPc0NkSNg0JxRXN5d+E7Sg==g0SNWTUmVNJnZgeK",
             },
         })
-          .then((response) => response.json())
-          .then((exercises) => console.log(exercises))
-          .catch((error) => console.log("error", error));
+            .then((response) => response.json())
+            .then((exercises) => {
+                exercises.forEach((exercise) => {
+                    // Print each exercise's details
+                    console.log(exercise);
+                });
+            })
+            .catch((error) => console.log("error", error));
     }
-    getAllExercises()
-    function getEachExercise(exercise){
-getAllExercises.forEach()
+    function getEachExercises(){
+        exercises.forEach((exercise) => {
+                    // Print each exercise's details
+                    console.log(exercise);
+                });
     }
-})
+    getAllExercises();
+});
+
+
+
+
+
+
   
 
