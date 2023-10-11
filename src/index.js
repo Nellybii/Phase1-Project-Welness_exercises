a253db3dc8de62a6493d013d640807978d26e298
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-11 16:55:25 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   let id = document.querySelector("#exers");
   let openDetails = null; // to track the currently open exercise details
-    console.log(button1);
+  let button1 = document.getElementById("btn");
+  //console.log(button1);
+// eventlistener to allow the user search on the desired exercise
+button1.addEventListener("click", (e) => {
+  // Get all the exercise detail elements with class "details"
+  const exerciseDetails = document.querySelectorAll(".details");
 
+  exerciseDetails.forEach((exerciseDetail) => {
+    // Find the difficulty within the exercise details
+    const difficultyElement = exerciseDetail.querySelector("li"[3]);
+   //console.log(difficultyElement)
+    if (difficultyElement) {
+      const difficulty = difficultyElement.textContent.replace("Difficulty: ", "");
+
+      if (difficulty === "beginner") {
+        exerciseDetail.style.display = "none";
+      } else {
+        exerciseDetail.style.display = "block";
+      }
+    }
+  });
+});
   function getAllExercises() {
     fetch("https://api.api-ninjas.com/v1/exercises", {
       method: "GET",
@@ -29,9 +49,37 @@ document.addEventListener("DOMContentLoaded", function () {
                   <li>Type: ${exercise.type}</li>
                   <li>Muscle: ${exercise.muscle}</li>
                   <li>Equipment: ${exercise.equipment}</li>
-                  <li>Difficulty: ${exercise.difficulty}</li>
-                  <li>Description: ${exercise.description}</li>
+                  <li id="diff">Difficulty: ${exercise.difficulty}</li>
+                  <li>Instruction: ${exercise.instructions}</li>
               `;
+          let div = document.createElement("div");
+          exerciseDetails.appendChild(div);
+          let btn1 = document.createElement("i");
+          div.appendChild(btn1);
+          btn1.innerHTML =`
+          <i class="fa fa-thumbs-up"></i>
+          `
+          btn1.style.color = "grey"
+          let btn2 = document.createElement("i");
+          div.appendChild(btn2);
+          btn2.innerHTML =`
+          <i class="fa fa-thumbs-down"></i>
+          `
+          btn2.style.color = "grey"
+          btn1.addEventListener("click",(e)=>{
+            e.preventDefault();
+            btn1.style.color = 'red'
+            if(btn1.style.color = 'red'){
+              btn2.style.color = "grey"
+            }
+          })
+          btn2.addEventListener("click",(e)=>{
+            e.preventDefault();
+            btn2.style.color = 'blue' 
+          if(btn2.style.color = 'blue'){
+            btn1.style.color = "grey"
+          }
+          })
 
           p.appendChild(exerciseDetails);
 
