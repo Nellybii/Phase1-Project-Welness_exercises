0711a12ec4b5c0ac35cb5517c85e513d2ae1027d
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-12 19:19:54 +0300

    event-listner

--- src/index.js
+++ src/index.js
@@ -8,35 +8,54 @@ document.addEventListener("DOMContentLoaded", function () {
       div.appendChild(exerciseList);
       exerciseList.innerHTML = exercise.name;
       let exerciseDetails = document.querySelector("#details");
- let beginner=document.querySelector("#btn")
-beginner.addEventListener("click",(e)=>{
-  e.preventDefault()
+      //console.log(exerciseDetails)
+      // let beginner = document.querySelector("#btn");
+      // beginner.addEventListener("click", (e) => {
+      //   e.preventDefault();
 
-   const beginnerExercise = exercises.filter((exercise)=>{
-exercise.difficulty === "beginner"
-   })
-   console.log(beginnerExercise)
-})
+      //   const beginnerExercise = exercises.filter((exercise) => {
+      //     exercise.difficulty === "beginner";
+      //   });
+      //   console.log(beginnerExercise);
+      // });
       exerciseList.addEventListener("click", (e) => {
         e.preventDefault();
-        //exerciseDetails.appendChild(ul);
- exerciseDetails.innerHTML = `
+        exerciseDetails.innerHTML = `
  <li>Type: ${exercise.type}</li>
  <li>Muscle: ${exercise.muscle}</li>
  <li>Equipment: ${exercise.equipment}</li>
  <li>Difficulty: ${exercise.difficulty}</li>
  <li>Instruction: ${exercise.instructions}</li>
- `
-    
+ `;
+        const icon = document.createElement("div");
+        exerciseDetails.appendChild(icon);
+        //console.log(icon)
+        icon.innerHTML = `
+    <i class="fa fa-thumbs-up" style="font-size:48px;color:grey"></i>
+    `;
+        const i = document.createElement("i");
+        i.innerHTML = `<i class="fa fa-thumbs-down" style="font-size:48px;color:grey"></i>`;
+
+        exerciseDetails.appendChild(i);
+        icon.addEventListener("click", (e) => {
+          e.preventDefault();
+          const clickedIcon = e.target;
+          clickedIcon.style.color = "red";
+        });
+        i.addEventListener("click", (e) => {
+          e.preventDefault();
+          const clickedi = e.target;
+          if(clickedi.style.color = "blue"){
+            icon.style.color = "grey"
+          }
+          else{
+            i.style.color = "grey"
+          }
+        });
       });
     });
   }
-  
-  let icon1 = document.createElement("i");
-  exerciseDetails.appendChild(icon1);
-  icon1.innerHTML = `
-  // <i class="fa fa-thumbs-up"></i>
-  //     `;
+
   // btn1.style.color = "grey";
   // let btn2 = document.createElement("i");
   // div.appendChild(btn2);
